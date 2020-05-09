18a3f776cbb2cb0678c5e5851cafe757f437f641
mkiriche2 <mkiriche2@gmail.com>
2020-05-09 13:56:00 -0500

    dynamic append

--- app.js
+++ app.js
@@ -15,17 +15,17 @@ getGeolocation();
       getBreweries(response.city);
     
 
-        mapboxgl.accessToken = 'pk.eyJ1IjoiY2FybG9zcmVtYTIiLCJhIjoiY2s5em5zZjB2MGN2bTNncDYyM2Ruc2FyZSJ9.piNzfWJ9-dRIsVM3le57gg';
-        var map = new mapboxgl.Map({
-        container: 'map', // container id
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: [response.longitude,response.latitude], // starting position
-        zoom: 9 // starting zoom
-    });
-        // Add zoom and rotation controls to the map.
-        map.addControl(new mapboxgl.NavigationControl());
-    });
-};
+            mapboxgl.accessToken = 'pk.eyJ1IjoiY2FybG9zcmVtYTIiLCJhIjoiY2s5em5zZjB2MGN2bTNncDYyM2Ruc2FyZSJ9.piNzfWJ9-dRIsVM3le57gg';
+            var map = new mapboxgl.Map({
+                container: 'map', // container id
+                style: 'mapbox://styles/mapbox/streets-v11',
+                center: [response.longitude, response.latitude], // starting position
+                zoom: 9 // starting zoom
+            });
+            // Add zoom and rotation controls to the map.
+            map.addControl(new mapboxgl.NavigationControl());
+          });
+        };
 
 function getBreweries(city){
    console.log(city);
@@ -55,19 +55,47 @@ $('#search').on('click', function(){
     var city = $('#searchBrewery').val();
     console.log(city);
     getBreweries(city.trim());
-})
-
-$('#searchBrewery').keypress(function (e) {
-    if (e.which == 13) {
-        var city = $('#searchBrewery').val();
-        console.log(city);
-        getBreweries(city.trim());
-      return false;
-    }
-  });
-
-
-
-
-
 });
+
+  
+    function getaddressLocation(nameBrewery, lat, long) {
+        console.log(nameBrewery);
+        console.log(lat);
+        console.log(long);
+        var queryURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + nameBrewery + '.json?proximity=-87.65,41.85&access_token=pk.eyJ1IjoiY2FybG9zcmVtYTIiLCJhIjoiY2s5em5zZjB2MGN2bTNncDYyM2Ruc2FyZSJ9.piNzfWJ9-dRIsVM3le57gg';
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+
+            $('body').append('<div>' + response.features[0].geometry.coordinates[0] + '</div>' + '  <div>' + response.features[0].geometry.coordinates[1] + '   </div>');
+            mapboxgl.accessToken = 'pk.eyJ1IjoiY2FybG9zcmVtYTIiLCJhIjoiY2s5em5zZjB2MGN2bTNncDYyM2Ruc2FyZSJ9.piNzfWJ9-dRIsVM3le57gg';
+
+            var map = new mapboxgl.Map({
+                container: 'map', // container id
+                style: 'mapbox://styles/mapbox/streets-v11',
+                center: [response.features[0].geometry.coordinates[0], response.features[0].geometry.coordinates[1]], // starting position
+                zoom: 13 // starting zoom
+            });
+            // Add zoom and rotation controls to the map.
+            map.addControl(new mapboxgl.NavigationControl());
+        });
+        };
+
+        $('#search').on('click', function () {
+            var city = $('#searchBrewery').val();
+            console.log(city);
+            getBreweries(city.trim());
+        });
+
+        $('#searchBrewery').keypress(function (e) {
+            if (e.which == 13) {
+                var city = $('#searchBrewery').val();
+                console.log(city);
+                getBreweries(city.trim());
+                return false;
+            };
+        });
+    });
